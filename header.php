eeb8afda3716eb5afe09860993aacf8ae3f3e2cc
Wren <wrenparker@gmail.com>
2021-04-28 13:53:17 -0400

    init

---
+++ header.php
@@ -0,0 +1,36 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Music App</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="/index.php">Music App</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="/index.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/new.php">New</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/popular.php">Popular</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" href="/search/" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+include "header.php";
+ ?>
+
+
+<div class="h-100 w-100" style="background-color: #f1f1f1">
+  <div class="container">
+    <div class="row pt-5 align-items-center">
+      <div class="col-md-4">
+        <div class="row">
+          <h2>Looking for music?</h2>
+        </div>
+        <div class="row">
+          <h3>Start listening to the best new releases.</h3>
+        </div>
+        <div class="row pt-3">
+          <a class="btn btn-success btn-block" href="/new.php" style="border-radius: 20px;" role="button">
+            New Releases
+          </a>
+        </div>
+        <div class="row pt-3">
+          <a class="btn btn-success btn-block" href="/popular.php" style="border-radius: 20px;" role="button">
+            Popular Albums
+          </a>
+        </div>
+      </div>
+      <div class="col-md-8">
+        <div class="row">
+          <div class="col-md-4">
+            <img src="./img/example.jpg" style="width:100%;" class="p-2" alt="uhoh">
+          </div>
+          <div class="col-md-4">
+            <img src="./img/example.jpg" style="width:100%;" class="p-2" alt="uhoh">
+          </div>
+          <div class="col-md-4">
+            <img src="./img/example.jpg" style="width:100%;" class="p-2" alt="uhoh">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4">
+            <img src="./img/example.jpg" style="width:100%;" class="p-2" alt="uhoh">
+          </div>
+          <div class="col-md-4">
+            <img src="./img/example.jpg" style="width:100%;" class="p-2" alt="uhoh">
+          </div>
+          <div class="col-md-4">
+            <img src="./img/example.jpg" style="width:100%;" class="p-2" alt="uhoh">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
